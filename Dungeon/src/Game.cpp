523c79fbd0befc858a9b0ebaa3a42083766ec3d7
assbee0 <55969823+assbee0@users.noreply.github.com>
2021-10-09 17:58:10 +0900

    Add files via upload

---
+++ Dungeon/src/Game.cpp
@@ -0,0 +1,76 @@
+﻿//! @file Game.cpp
+
+#include	<SDL/SDL.h>
+#include	<Game.h>
+
+namespace Dungeon
+{
+	Game::Game():
+		mWindow(nullptr),
+		mIsRunning(true)
+	{
+
+	}
+
+	bool Game::Initialize()
+	{
+		// SDL库初始化
+		if (SDL_Init(SDL_INIT_VIDEO) != 0)
+		{
+			SDL_Log("SDL库初始化失败: %s", SDL_GetError());
+			return false;
+		}
+
+		// 窗口初始化
+		mWindow = SDL_CreateWindow("Dungeon", 400, 200, 600, 480, 0);
+		if (!mWindow)
+		{
+			SDL_Log("窗口初始化失败: %s", SDL_GetError());
+			return false;
+		}
+
+		return true;
+	}
+
+	void Game::Loop()
+	{
+		while (mIsRunning)
+		{
+			Event();
+			Update();
+			Draw();
+		}
+	}
+
+	void Game::Shutdown()
+	{
+		SDL_DestroyWindow(mWindow);
+		SDL_Quit();
+	}
+
+	void Game::Event()
+	{
+		SDL_Event event;
+		// 如果事件队列中有未处理的事件，按顺序处理
+		while (SDL_PollEvent(&event))
+		{
+			switch (event.type)
+			{
+				// 退出事件，按下窗口的叉
+			case SDL_QUIT:
+				mIsRunning = false;
+				break;
+			default:
+				break;
+			}
+		}
+	}
+
+	void Game::Update()
+	{
+	}
+
+	void Game::Draw()
+	{
+	}
+}
