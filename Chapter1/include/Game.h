80a3044488afd27727754dc76f5657449b874dd0
assbee0 <55969823+assbee0@users.noreply.github.com>
2021-10-09 18:02:02 +0900

    Add files via upload

---
+++ Chapter1/include/Game.h
@@ -0,0 +1,34 @@
+﻿//! @file Game.h
+
+#ifndef __Dungeon_Game__
+#define __Dungeon_Game__
+
+namespace Dungeon
+{
+	class Game
+	{
+	public:
+		//!	构造函数
+		Game();
+
+		//!	初始化
+		bool	Initialize();
+		//!	主循环
+		void	Loop();
+		//!	游戏结束
+		void	Shutdown();
+
+	private:
+		//!	处理事件
+		void	Event();
+		//!	更新各种状态
+		void	Update();
+		//!	渲染
+		void	Draw();
+
+		class SDL_Window*	mWindow;		//!<	窗口
+		bool				mIsRunning;		//!<	运行状态
+	};
+}
+
+#endif	// __Dungeon_Game__
