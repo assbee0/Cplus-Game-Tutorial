879117e957dc739ffbe70332a15e03a93f42af0a
assbee0 <55969823+assbee0@users.noreply.github.com>
2021-10-14 20:36:06 +0900

    Add files via upload

---
+++ Chapter4/include/GameObject.h
@@ -0,0 +1,46 @@
+﻿//! @file GameObject.h
+
+#ifndef __Dungeon_GameObject__
+#define __Dungeon_GameObject__
+
+#include	<string>
+#include	<vector>
+
+namespace Dungeon
+{
+	class GameObject
+	{
+	public:
+		//! 物体状态
+		enum class State
+		{
+			EActive,
+			EDead
+		};
+
+		//! 构造函数
+		GameObject(class Game* game);
+		//! 析构函数
+		virtual ~GameObject();
+
+		//! 每帧更新
+		void	Update();
+		//! 添加组件
+		void	AddComponent(class Component* component);
+		//! 移除组件
+		void	RemoveComponent(class Component* component);
+
+		//! 物体状态的get和set
+		State	GetState() const;
+		void	SetState(State state);
+
+	protected:
+		std::vector<class Component*>	mComponents;	//!<	组件数组
+
+	private:
+		class Game*						mGame;			//!<	Game对象
+		State							mState;			//!<	物体当前状态
+	};
+}
+
+#endif	// __Dungeon_GameObject__
